5879ea34b4f1cd3361792c5a19fd9391c831ca14
Samer Buna <samer.buna@gmail.com>
2014-11-18 14:17:13 -0800

    CB2 test

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var crunchbase = require('crunchbase2');
+crunchbase.init(process.env.CB_API_KEY);
+
+crunchbase.organizations( { query: "airbnb" } , function(error, results) {
+  console.log(results.data.items[0]);
+});
 
 router.get('/', function(req, res) {
   res.send("Crunchbase API local wrapper");
+});
+
+router.get('/organizations', function(req, res) {
+  // crunchbase.organizations( { query: "airbnb" } , function(error, results) {
+  // });
 });
 
 module.exports = router;
