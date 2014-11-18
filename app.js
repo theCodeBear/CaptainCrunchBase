ac4a4116cf4465dd147b7ad208bc2f0477245543
Samer Buna <samer.buna@gmail.com>
2014-11-18 13:28:05 -0800

    crunchbase and an api routes file

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var apiv1 = require('./routes/apiv1');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/apiv1', apiv1);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
