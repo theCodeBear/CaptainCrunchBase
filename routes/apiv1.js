2897b63021f67a69b2c3763bac25e8abf0ad1c95
Samer Buna <samer.buna@gmail.com>
2014-11-18 14:27:27 -0800

    organizations api wrap

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -3,17 +3,15 @@ var router = express.Router();
 var crunchbase = require('crunchbase2');
 crunchbase.init(process.env.CB_API_KEY);
 
-crunchbase.organizations( { query: "airbnb" } , function(error, results) {
-  console.log(results.data.items[0]);
-});
-
 router.get('/', function(req, res) {
   res.send("Crunchbase API local wrapper");
 });
 
 router.get('/organizations', function(req, res) {
-  // crunchbase.organizations( { query: "airbnb" } , function(error, results) {
-  // });
+  // req.query: { query: "sdafs", name: "asdfsad" }
+  crunchbase.organizations( req.query , function(error, results) {
+    res.json(results.data.items);
+  });
 });
 
 module.exports = router;
