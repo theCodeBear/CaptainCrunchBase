ce9d40be8202ab8e448904dde405a33e299772f4
Samer Buna <samer.buna@gmail.com>
2014-11-18 17:20:40 -0800

    Organization API and store in Mongo

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -3,14 +3,27 @@ var router = express.Router();
 var crunchbase = require('crunchbase2');
 crunchbase.init(process.env.CB_API_KEY);
 
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGOHQ_URL);
+var CompanySchema = new mongoose.Schema({}, { strict: false });
+var CompanyModel = mongoose.model("Company", CompanySchema);
+
 router.get('/', function(req, res) {
   res.send("Crunchbase API local wrapper");
 });
 
 router.get('/organizations', function(req, res) {
-  // req.query: { query: "sdafs", name: "asdfsad" }
   crunchbase.organizations( req.query , function(error, results) {
     res.json(results.data.items);
+  });
+});
+
+router.get('/organization/:permalink', function(req, res) {
+  crunchbase.organization( req.params.permalink , function(error, results) {
+    var company = new CompanyModel(results);
+    company.save();
+
+    res.json(results);
   });
 });
 
