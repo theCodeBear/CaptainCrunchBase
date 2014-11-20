af83af6c87cc0801fb04682d89cb3505ad7320a0
Samer Buna <samer.buna@gmail.com>
2014-11-19 17:35:31 -0800

    Prep for org info div

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,9 +9,21 @@ app.config(function($routeProvider) {
 });
 
 app.controller("MainController", function($http, $scope) {
+  $scope.collapse = function(clickedOrg) {
+    // get the clickedOrg info, and display it in the details div
+    // use /apiv1/organization/{the permalink of org, which is in clickedOrg.path
+    $scope.orgs.forEach(function(e) {
+      e.visible = (e === clickedOrg);
+    });
+  }
+
   $scope.searchCCB = function() {
     $http.get("/apiv1/organizations?query=" + $scope.searchQuery).then(function(response) {
       $scope.orgs = response.data;
     });
   }
+
+  $scope.$watch("searchQuery", function() {
+    // $scope.orgs = [];
+  });
 });
