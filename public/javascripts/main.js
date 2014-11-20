aee28687af676f616648cf61bcbb464e95f22360
Todd Kronenberg <toddkronenberg@gmail.com>
2014-11-19 23:57:27 -0800

    Make it so it does not make extra calls to CrunchBase API if an organization was already clicked in a given session of the app.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,5 @@
 var app = angular.module('ccb', ['ngRoute']);
+var loadedOrgs = [];
 
 app.config(function($routeProvider) {
   $routeProvider
@@ -10,11 +11,39 @@ app.config(function($routeProvider) {
 
 app.controller("MainController", function($http, $scope) {
   $scope.collapse = function(clickedOrg) {
+    $scope.dataLoaded = false;
     // get the clickedOrg info, and display it in the details div
     // use /apiv1/organization/{the permalink of org, which is in clickedOrg.path
+
+    // If company, when clicked on from list, is already in database then load
+    // from database rather than making another call to the CrunchBase API
+
+    var prevClicked = -1;
+    loadedOrgs.forEach(function(e,i) {
+      if (clickedOrg.name == e.name) prevClicked = i;
+    });
+    // if company has already been clicked on during this viewing of the app
+    // then call the company info from the loadedOrgs variable rather than making
+    // another get request to the CrunchBase API.
+    if (prevClicked > -1) {
+      $scope.details = loadedOrgs[prevClicked];
+      $scope.dataLoaded = true;
+      console.log($scope.details);
+    } else {      // make call to CrunchBase API through our API
+      $http.get("/apiv1/" + clickedOrg.path).then(function(response) {
+        $scope.details = response.data.data.properties;
+        $scope.dataLoaded = true;
+        loadedOrgs.push($scope.details);
+        console.log($scope.details);
+      });
+    }
     $scope.orgs.forEach(function(e) {
       e.visible = (e === clickedOrg);
     });
+  }
+
+  function dataLoaded() {
+
   }
 
   $scope.searchCCB = function() {
@@ -24,6 +53,6 @@ app.controller("MainController", function($http, $scope) {
   }
 
   $scope.$watch("searchQuery", function() {
-    // $scope.orgs = [];
+    $scope.orgs = [];
   });
 });
